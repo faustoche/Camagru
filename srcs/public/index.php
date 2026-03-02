d3f2b12b90db53a629b96d985fa34dcf89b6c6e5
faustoche <faustine.crocq@gmail.com>
2026-03-02 12:39:01 +0100

    feat(view) add new view

--- srcs/public/index.php
+++ srcs/public/index.php
@@ -1,2 +1,18 @@
 <?php
-echo "Camagru fonctionne !";
+
+## Démarrage de la session
+Session::start();
+
+## Chargement des 3 classes nécéssaires
+require_once __DIR__ . '/../core/Router.php';
+require_once __DIR__ . '/../core/Session.php';
+require_once __DIR__ . '/../core/Auth.php';
+
+## On instancie le routeur
+$routeur = new Router();
+
+## Définition de la route 
+$router->get('/', 'HomeController', 'index');
+
+## Lancement du programme/site 
+$router->resolve();
---
+++ srcs/views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>CAMAGRU</title>
+	</head>
+
+	<body>
+		<h1>Bienvenue sur Camagru</h1>
+	</body>
+</html>
